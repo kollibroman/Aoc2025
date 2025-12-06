52c6710d3e570e35a7e462e79bfb7cb91d2ce732
Filip <filipkwiek888@gmail.com>
2025-12-06 13:10:57 +0100

    Day 1 completed

---
+++ Aoc2025/Day1.cs
@@ -0,0 +1,122 @@
+namespace Aoc2025;
+
+public class Day1
+{
+    private static async Task<List<string>> GetInputList()
+    {
+        var inputArr = await File.ReadAllLinesAsync("/home/kollibroman/LosoweProjekty/Aoc2025/Aoc2025/Input/Day1.txt");
+        return inputArr.ToList();    
+    }
+    
+    public static async Task<int> Part1Solution()
+    {
+        var numbersList = Enumerable.Range(0, 100).ToList();       
+        
+        var inputList = await GetInputList();
+        
+        var startPos = numbersList.IndexOf(50);
+        
+        var zerosInSeq = 0;
+        
+        foreach (var input in inputList)
+        {
+            var direction = input[0];
+            var distance = int.Parse(input[1..]);
+            
+            switch (direction)
+            {
+                case 'R':
+                {
+                    startPos += (distance % 100);
+
+                    var isIndexOutOfRange = startPos > numbersList.Count - 1;
+                
+                    if (isIndexOutOfRange)
+                    {
+                        startPos %= 100;
+                    }
+
+                    break;
+                }
+                case 'L':
+                {
+                    startPos -= distance;
+
+                    var isIndexOutOfRange = startPos < 0;
+
+                    if (isIndexOutOfRange)
+                    {
+                        startPos = ((startPos % 100) + 100) % 100;
+                    }
+
+                    break;
+                }
+            }
+
+            if (numbersList[startPos] == 0)
+            {
+                zerosInSeq++;
+            }
+        }
+        
+        return zerosInSeq;
+    }
+    
+    // IT WORKED BUT AT WHAT COST 
+    public static async Task<int> Part2Solution()
+    {
+        var numbersList = new  List<int>();
+
+        for (int i = 0; i < 999; i++)
+        {
+            numbersList.AddRange(Enumerable.Range(0, 100).ToList());
+        }
+        
+        var inputList = await GetInputList();
+        
+        var startPos = 52950;
+        var startPosNum = numbersList[52950];
+        
+        var zerosDuringRotation = 0;
+        
+        foreach (var input in inputList)
+        {
+            var direction = input[0];
+            var distance = int.Parse(input[1..]);
+            
+            switch (direction)
+            {
+                case 'R':
+                {
+                    for (int i = 0; i < distance; i++)
+                    {
+                        startPos++;
+
+                        if (numbersList[startPos] == 0)
+                        {
+                            zerosDuringRotation++;
+                        }
+                    }   
+
+                    break;
+                }
+                case 'L':
+                {
+                    for (int i = 0; i < distance; i++)
+                    {
+                        startPos--;
+
+                        if (numbersList[startPos] == 0)
+                        {
+                            zerosDuringRotation++;
+                        }
+                    }   
+
+                    break;
+                }
+            }
+        }
+        
+        return zerosDuringRotation;
+    }
+}
---
+++ Aoc2025/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Aoc2025;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var solutionPart1 = await Day1.Part1Solution();
+        var solutionPart2 = await Day1.Part2Solution();
+        
+        Console.WriteLine(solutionPart1);
+        Console.WriteLine(solutionPart2);
+    }
+}
